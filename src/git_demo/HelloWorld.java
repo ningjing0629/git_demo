675cf6fd1cfe14e51999a50578862567f5a134ca
ningjing0629 <0629jing>
2018-06-30 01:06:01 +0800

    modify

--- src/git_demo/HelloWorld.java
+++ src/git_demo/HelloWorld.java
@@ -3,6 +3,8 @@ package git_demo;
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("hello egit");
+		System.out.println("hello egit2");
+		System.out.println("hello egit3");
 	}
 
 }
